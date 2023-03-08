4682cb0bd95d2ed591e15a0238a93ebcebb40a9b
Tiia Pitkänen <tiia1.pitkanen@gmail.com>
2023-03-08 13:52:19 +0100

    added data

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,17 @@
-let temp = document.querySelector('#temp');
-
-let currentCity = '';
-let currentDate = '';
-let currentTime = '';
-let lat = '';
-let long = '';
+const timeEl = document.querySelector('#time');
+const dateEl = document.querySelector('#date');
+const tempEl = document.querySelector('#temp');
+const APIkey = '6974a09c36ef8aaabfc009e4231a867d';
+let cityEl = document.querySelector('#city');
+let weatherIcon = document.querySelector('.weather-icon');
+let highlightEl = document.querySelector('.highlight-container');
+let descEl = document.querySelector('.weather-description');
 
 //Getting the date and time
-function getDateTime() {
-	const dateContainer = document.querySelector('#date-time');
-
-	let now = new Date(),
-		hour = now.getHours(),
-		minute = now.getMinutes();
+setInterval(() => {
+	let now = new Date();
+	let hour = now.getHours();
+	let minute = now.getMinutes();
 
 	let days = [
 		'Sunday',
@@ -24,6 +23,20 @@ function getDateTime() {
 		'Saturday',
 	];
 
+	let months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'October',
+		'November',
+		'December',
+	];
+
 	hour = hour % 24;
 	if (hour < 10) {
 		hour = '0' + hour;
@@ -32,40 +45,103 @@ function getDateTime() {
 		minute = '0' + minute;
 	}
 
-	
-
 	let dayString = days[now.getDay()];
+	let monthString = months[now.getMonth()];
+	let dayNro = now.getUTCDate();
 
-	dateContainer.innerHTML = `${dayString}, ${hour}:${minute}`;
+	timeEl.innerHTML = `${hour}:${minute}`;
+	dateEl.innerHTML = `${dayString}, ${dayNro} ${monthString} `;
 
 	return `${dayString}, ${hour}:${minute}`;
+}, 1000);
+
+function getWeatherData() {
+	navigator.geolocation.getCurrentPosition((success) => {
+		let { latitude, longitude } = success.coords;
+
+		fetch(
+			`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${APIkey}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				showWeatherData(data);
+			});
+	});
 }
 
-//WeatherData
+getWeatherData();
 
-const tempContainer = document.querySelector('#temp');
-const cityContainer = document.querySelector('#city');
-const form = document.querySelector('.search');
+function showWeatherData(data) {
+	let city = data.city.name;
+	let country = data.city.country;
+	let temp = Math.floor(data.list[0].main.temp);
+	let wind = data.list[0].wind.speed;
+	let { temp_min, temp_max, feels_like, humidity, pressure } =
+		data.list[0].main;
+	let { main } = data.list[0].weather[0];
+	let icon = '';
 
-form.addEventListener('submit', function (event) {
-	event.preventDefault();
+	let temp_min_floor = Math.floor(temp_min);
+	let temp_max_floor = Math.floor(temp_max);
+	icon =
+		main === 'Clear'
+			? 'assets/icons/sun.png'
+			: main === 'Clouds'
+			? 'assets/icons/cloud-computing.png'
+			: main === 'Snow'
+			? 'assets/icons/snow.png'
+			: main === 'Thunderstorm'
+			? 'assets/icons/storm.png'
+			: main === 'Drizzle' || main === 'Rain'
+			? 'assets/icons/rain.png'
+			: 'assets/icons/foggy.png';
 
-	let input = document.querySelector('#query');
-	let inputValue = input.value;
-	console.log(inputValue);
+	cityEl.innerHTML = `${city}, ${country}`;
+	tempEl.innerHTML = `${temp}`;
 
-	currentCity = inputValue;
+	highlightEl.innerHTML = `<div class="card2">
+							<h4 class="card-heading">Feels Like</h4>
+							<div class="content">
+								<p class="fl-temp">${feels_like}</p>
+								<span class="fl-temp-unit">°C</span>
+							</div>
+						</div>
+						<div class="card2">
+							<h4 class="card-heading">Wind</h4>
+							<div class="content">
+								<p class="wind">${wind}</p>
+								<span class="wind-unit">m/s</span>
+							</div>
+						</div>
+						<div class="card2">
+							<h4 class="card-heading">Humidity</h4>
+							<div class="content">
+								<p class="humidity">${humidity}</p>
+								<span class="humidity-unit">%</span>
+							</div>
+						</div>
+						<div class="card2">
+							<h4 class="card-heading">Pressure</h4>
+							<div class="content">
+								<p class="pressure">${pressure}</p>
+							</div>`;
 
-	fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=6974a09c36ef8aaabfc009e4231a867d&units=metric`,
-		{
-			method: 'GET',
-		}
-	)
-		.then((response) => response.json())
-		.then((data) => {
-			temp = data.main.temp;
-			tempContainer.innerHTML = temp;
-			cityContainer.innerHTML = currentCity;
-		});
-});
+	descEl.innerHTML = `<div class="description">
+						<p id="description"> ${main}</p>
+					</div>
+					<div class="min-max">
+						<div class="min">
+							<p>Lowest:</p>
+							<p id="min-temp">${temp_min_floor}</p>
+							<span class="temp-unit">°C</span>
+						</div>
+						<div class="max">
+							<p>Highest:</p>
+							<p id="max-temp">${temp_max_floor}</p>
+							<span class="temp-unit">°C</span>
+						</div>
+					</div>`;
+
+	weatherIcon.innerHTML = `<img src="${icon}" class="icon-large" id="icon" alt="" />`;
+}
